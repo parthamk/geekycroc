0c36eb313107c56b0691229b4326375ec1213bd6
parthamk <partha0988@gmail.com>
2023-05-16 16:58:00 +0530

    first commit

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright © GEEKYCORC all rights reserved. Coded by ParthaMk.</p>
+        <em><a href="https://www.youtube.com/watch?v=Wahz9bZfpv8">Tutorials by GorkCoder</a></em>
+      </footer>
+    </>
+  )
+}
